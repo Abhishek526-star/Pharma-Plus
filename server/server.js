03239e2cffa3a4de2b06d3cc3d0e4669ca0ac480
Abhishek Kumar <abhishekkumar63871@gmail.com>
2026-08-02 12:40:42 +0530

    Update CORS configuration to remove default localhost origin

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ connectDB();
 // Security & Parsing Middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL ,
     credentials: true,
 }));
 
